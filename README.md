fbf7576a592a1c108630653f2576c5239f5e5f4f
Madhav Khandhar <madhavkhandhar@gmail.com>
2021-03-15 02:16:32 +0800

    First Commit and Initial Setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# P2PMC Client Mod
+
+## Setup
+
+Run gradlew build to build the mod.
+Run gradlew runClient to start the client
---
+++ src/main/java/net/fabricmc/p2pmc/P2pHead.java
@@ -0,0 +1,11 @@
+package net.fabricmc.p2pmc;
+
+import net.fabricmc.api.ModInitializer;
+
+public class P2pHead implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("This is peer to peer minecraft");
+    }
+    
+}
